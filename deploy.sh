6590d41044b84ef273a1899c58bb45d5e36b143f
Nguyễn Ngọc Quí <69713999+ngocqui1609@users.noreply.github.com>
2022-08-26 11:03:32 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# cd `dirname $BASH_SOURCE`
+cd `dirname $BASH_SOURCE`
 
-docker-compose down
+docker-compose stop
+docker-compose rm -f
 docker-compose pull
 docker-compose up -d
 
-# cd - > /dev/null
+cd - > /dev/null
